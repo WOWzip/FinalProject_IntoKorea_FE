d1b347e106d7145989b149457ff2d8a6cf0eda87
cheolll <wkdcjf9204@gmail.com>
2024-02-07 15:28:14 +0900

    리스트페이지

--- src/components/pages/fragnents/header.jsx
+++ src/components/pages/fragnents/header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 
 function Header(props){
     return(
         <>
             <h1>헤더입니다.</h1>
+            <Link to="/">메인페이지로</Link>
         </>
     )
 }
--- src/components/pages/tours/toursListApi.jsx
+++ src/components/pages/tours/toursListApi.jsx
@@ -3,7 +3,11 @@ import axios from 'axios'
 import TourItem from "./touritem";
 import styled from "styled-components";
 import Pagination from "react-js-pagination"
-import TourCategory from "./tourCategory";
+import TourHeader from "./tourHeader";
+import AreaCode from "./areaCode1/areaCode";
+import CategoryCode from "./categoryCode1/categoryCode";
+import ContentsTypeId from "./contentsTypeId/contentsTypeId";
+import DetailAreaCode from "./areaCode1/detailAreaCode";
  
 
 
@@ -46,10 +50,22 @@ const PaginationBox = styled.div`
 
 const ToursListApi = () => {
     const [loading, setLoading] = useState(false);
+    const startTime = new Date();
+
     const [datas, setData] = useState(null);
     const [page, setPage] = useState(1);
     const [totalData, setTotalData] = useState(1);
     const itemsCountPerPage = 10;
+
+    const [areaCode , setAreaCode] = useState("") ;
+    const [categoryCode, setCategoryCode] = useState("");
+    const [contentsTypeId, setContentsTypeId] = useState("");
+    // 시군구 코드
+    const [ showDetailAreaCode, setShowDetailAreaCode ] = useState("");
+    const [ detailAreaCode, setDetailAreaCode ] = useState("");
+
+
+    // 태그 클릭시 요청값 저장
 
     const key = 'wGgAMNctzAVjo7O4ZlwZPcCNHPr9t8IPlm4lYhfG1RbY79FR2pL%2BnAhWAyP0%2FObPwgvONXIi1Ke1UTRujCO%2Fnw%3D%3D'
     
@@ -61,26 +77,62 @@ const ToursListApi = () => {
             try {
                 const response = await axios.get(
                     // 	지역기반관광정보조회 - areaBasedList1
-                    `http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=${itemsCountPerPage}&pageNo=${page}&MobileOS=ETC&MobileApp=AppTest&_type=json&ServiceKey=${key}&listYN=Y&arrange=A&contentTypeId=&areaCode=&sigunguCode=&cat1=&cat2=&cat3=`
-                );
+                    `http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=${itemsCountPerPage}&pageNo=${page}&MobileOS=ETC&MobileApp=AppTest&_type=json&ServiceKey=${key}&listYN=Y&arrange=O&contentTypeId=${contentsTypeId}&areaCode=${areaCode}&sigunguCode=${detailAreaCode}&cat1=${categoryCode}&cat2=&cat3=`
+                    );
+                const endTime = new Date();
+                const loadingTime = endTime - startTime;
+                console.log(`로딩 시간: ${loadingTime}ms`);
+                // setData(response)
                 setData(response.data.response.body.items.item);
-                console.log(response.data.response.body.totalCount)
-                console.log(response.data)
-                console.log(response.data.response.body.items.item)
-                console.log(page)
+                console.log(response)
+
 
                 setTotalData(response.data.response.body.totalCount);
-                
                 setLoading(false);
+
+
             } catch (error) {
                 console.log(error);
                 setLoading(false);
             }
         }
         fetchData();
-    },[page])
+        // eslint-plugin-react-hooks에서는 의존성 배열에 모든 외부 변수를 포함하도록 요구함
+        // 의존성 해달 줄에서만 의존성 배열을 무시하도록 지시
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[page, areaCode, categoryCode, contentsTypeId, detailAreaCode, showDetailAreaCode])
+
     
     const handlePageChange = ( page ) =>{ setPage(page)}
+
+    const handleFilterArea = (code) => {
+        setAreaCode(code);
+        setDetailAreaCode("");
+        console.log("코드값 : ", code)
+
+        if(code !== ''){
+            setShowDetailAreaCode(<DetailAreaCode code={code} onClick={handleFilterDetailArea}/>)
+            console.log("이거 나와야해 " ,detailAreaCode)
+        } else {
+            setShowDetailAreaCode(null);
+        }
+    }
+
+    
+    const handleFilterDetailArea = (code) => {
+        setDetailAreaCode(code)
+    }
+
+    const handleFilterCategory = (code) => {
+        setCategoryCode(code)
+    }
+
+    const handleFilterContentsTypeId = (code) => {
+        setContentsTypeId(code)
+    }
+
+
+
 
     // 대기 중일때
     if(loading){
@@ -93,9 +145,22 @@ const ToursListApi = () => {
     }
 
     return (
-        <>
+        <>  
+            <div>
+                값 확인
+                <h4>areaCode : {areaCode} , categoryCode : {categoryCode} , 
+                contentsTypeId : {contentsTypeId} , detailAreaCode : {detailAreaCode}</h4>
+            </div>
+            {/* 버튼을 누르면 버튼의 값으로 api 요청 */}
+            <AreaCode onClick={handleFilterArea}/>
+            {showDetailAreaCode}
+
+            <CategoryCode onClick={handleFilterCategory}/>
+
+            <ContentsTypeId onClick={handleFilterContentsTypeId} />
+
             <TourListBlock>
-            <TourCategory />
+            <TourHeader />
                 {datas.map((data, index) => (
                         <TourItem key={data.firstimage || index } data={data} />
                 ))}
