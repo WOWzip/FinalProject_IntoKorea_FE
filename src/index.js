6f920c17eb931dbc9ad6ba95027df131dc05f8ad
Front-End <wodnr4135529@naver.com>
2024-02-07 15:38:41 +0900

    hi

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/pages/App';
+import {Route, Routes} from 'react-router-dom'
+import MyPage from './components/pages/mypage/MyPage';
+import QnA from './components/pages/mypage/QnA';
+import Question from './components/pages/mypage/Question';
+import History from './components/pages/mypage/History';
+import Keep from './components/pages/mypage/Keep';
+import MyPageSidebar from './components/MyPageSidebar';
+import Traveldiary from './components/pages/mypage/TravelDiary';
+import EditQuestion from './components/pages/mypage/EditQuestion';
+import QnAdetail from './components/pages/mypage/QnAdetail';
+
+
 import Header from './components/pages/fragnents/header';
 import Footer from './components/pages/fragnents/footer';
 
@@ -12,5 +23,23 @@ root.render(
     <Header />
     <App/>
     <Footer />
+    <div style={{ display: 'flex' }}>
+      <MyPageSidebar />
+      <Routes>
+        <Route index element={<MyPage/>} />
+        <Route path="/QnA" element={<QnA />} />
+        <Route path="/Question" element={<Question />} />
+        <Route path="/History" element={<History />} />
+        <Route path="/Keep" element={<Keep />} />
+        <Route path="/Traveldiary" element={<Traveldiary/>}/>
+        <Route path="/EditQuestion" element={<EditQuestion/>}/>
+        <Route path="/QnAdetail" element={<QnAdetail/>}/>
+      </Routes>
+    </div>
   </BrowserRouter>
 );
+
+
+
+
+
