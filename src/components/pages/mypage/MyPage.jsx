a88652fb90df79c9acb803de29e2f79d9befbf3f
Front-End <wodnr4135529@naver.com>
2024-02-19 11:15:33 +0900

    g

--- src/components/pages/mypage/MyPage.jsx
+++ src/components/pages/mypage/MyPage.jsx
@@ -31,26 +31,7 @@ const MyPage = () => {
                 </div>
                 <div className="keep-container box">
                     <form>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
+
                         <button className="keepbutton">
                             <Link to="/BookMark" className="link">즐겨찾기</Link>
                         </button>
@@ -59,26 +40,7 @@ const MyPage = () => {
 
                 <div className="history-container box">
                     <form>
-                    <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
+
                         <button className="historybutton">
                             <Link to="/History" className="link">여행 일기</Link>
                         </button>
@@ -87,26 +49,7 @@ const MyPage = () => {
                 
                 <div className="qna-container box">
                     <form>
-                    <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
+
                         <button className="qnabutton">
                             <Link to="/QnA"  className="link">Q&A</Link>
                         </button>
